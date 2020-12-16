ca3cba796df792bf954ba206a519afe5fd4b8958
Vyacheslav Gorin <amneduet@gmail.com>
2020-12-16 18:12:13 +0200

    Initial File Upload

---
+++ src/index.js
@@ -0,0 +1,215 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+exports.__esModule = true;
+var amqp = require('amqplib/callback_api');
+var AmqpClient = /** @class */ (function () {
+    //Public methods
+    function AmqpClient(conn_string, queue_list, consumer_list) {
+        if (conn_string === void 0) { conn_string = ""; }
+        if (queue_list === void 0) { queue_list = []; }
+        if (consumer_list === void 0) { consumer_list = []; }
+        this._connectionString = "";
+        this._connectionPending = false;
+        this._statusQueueInitialized = false;
+        this._connection = null;
+        this._channel = null;
+        this._queueList = [];
+        this._consumerList = [];
+        this._logPrefix = "[AMQP_CLIENT]";
+        this._statusQueue = "AMQP_CLIENT_STATUS";
+        this._connectionString = conn_string;
+        this._queueList = queue_list;
+        this._consumerList = consumer_list;
+        this._selfId = this._createSelfId();
+    }
+    AmqpClient.prototype.setConnectionString = function (conn_string) {
+        this._connectionString = conn_string;
+    };
+    AmqpClient.prototype.setStatusQueueName = function (queue_name) {
+        this.addQueue(queue_name);
+        this._statusQueueInitialized = true;
+        this._statusQueue = queue_name;
+    };
+    AmqpClient.prototype.addQueue = function (queue_name, options) {
+        if (options === void 0) { options = { durable: false }; }
+        this._queueList.push({ name: queue_name, options: options });
+    };
+    AmqpClient.prototype.addConsumer = function (queue_name, callback, options) {
+        if (options === void 0) { options = { noAck: true }; }
+        this._consumerList.push({ name: queue_name, callback: callback, options: options });
+    };
+    //Perform a connection with defined queues and consumers
+    AmqpClient.prototype.connect = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var validate;
+            var _this = this;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        validate = true;
+                        if (this._queueList.length == 0) {
+                            validate = false;
+                            this.log("[\x1b[31m✘\x1b[0m] Queue list shouldn't be empty when opening a new connection");
+                        }
+                        if (this._consumerList.length == 0) {
+                            validate = false;
+                            this.log("[\x1b[31m✘\x1b[0m] Consumer list shouldn't be empty when opening a new connection");
+                        }
+                        if (!validate) return [3 /*break*/, 2];
+                        this._connectionPending = true;
+                        return [4 /*yield*/, amqp.connect(this._connectionString, function (err, conn) { return _this.connectionCallback(err, conn); })];
+                    case 1:
+                        _a.sent();
+                        _a.label = 2;
+                    case 2:
+                        if (!this._statusQueueInitialized) {
+                            this.addQueue(this._statusQueue);
+                        }
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    AmqpClient.prototype.sendMessage = function (queue, data) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                if (this._connection) {
+                    try {
+                        this._channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
+                    }
+                    catch (err) {
+                        this.log("[\x1b[31m✘] Error while sending message");
+                        this._connection = null;
+                        this._channel = null;
+                        this.connect();
+                    }
+                }
+                return [2 /*return*/];
+            });
+        });
+    };
+    //Redefine this method, if you need to have your own logging
+    AmqpClient.prototype.log = function (content) {
+        var args = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            args[_i - 1] = arguments[_i];
+        }
+        console.log.apply(console, __spreadArrays([this._logPrefix, content.toString()], args));
+    };
+    AmqpClient.prototype.setLogPrefix = function (prefix) {
+        this._logPrefix = prefix;
+    };
+    AmqpClient.prototype.updateConnectionStatus = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                this._updateClientStatus();
+                return [2 /*return*/];
+            });
+        });
+    };
+    //Private methods
+    AmqpClient.prototype.connectionCallback = function (err, conn) {
+        var _this = this;
+        if (err) {
+            this.log("Connection error: ", err.toString());
+        }
+        else {
+            this._connection = conn;
+            this._connection.createChannel(function (err, channel) { return _this.channelCallback(err, channel); });
+            this.log("[\x1b[32m✔\x1b[0m] Connection established");
+            this.log("Client Self ID is \u001B[33m" + this._selfId + "\u001B[0m");
+        }
+        this._connectionPending = false;
+    };
+    //This metod is called, when the connection is received in connect method
+    //Each pre-defined queue and consumer are asserted or connected to the channel
+    AmqpClient.prototype.channelCallback = function (err, channel) {
+        var _this = this;
+        if (err) {
+            this.log("[\x1b[31m✘\x1b[0m] Error while creating channel: ", err.toString());
+        }
+        else {
+            this._channel = channel;
+            //Connecting queues defined in list
+            this._queueList.forEach(function (queue) {
+                _this._channel.assertQueue(queue.name, queue.options);
+            });
+            //Connecting consumers defined in list
+            this._consumerList.forEach(function (consumer) {
+                _this._channel.consume(consumer.name, consumer.callback, consumer.options);
+            });
+        }
+    };
+    AmqpClient.prototype._updateClientStatus = function () {
+        if (this._connection) {
+            try {
+                this._channel.sendToQueue(this._statusQueue, Buffer.from(JSON.stringify({ clientId: this._selfId, dt: new Date() })));
+            }
+            catch (err) {
+                this.log(err.toString());
+                this.log("[\x1b[31m✘\x1b[0m] Connection lost. Reconnecting");
+                this._connection = null;
+                this._channel = null;
+                this.connect();
+            }
+        }
+        else if (!this._connectionPending) {
+            this._connection = null;
+            this._channel = null;
+            this.connect();
+        }
+    };
+    AmqpClient.prototype._createSelfId = function (count) {
+        if (count === void 0) { count = 4; }
+        var s = [];
+        for (var i = 0; i < count; i++) {
+            s[i] = "0123456789abcdef".substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        var s_id = s.join("");
+        return s_id;
+    };
+    return AmqpClient;
+}());
+exports["default"] = AmqpClient;
---
+++ src/index.ts
@@ -0,0 +1,162 @@
+const amqp = require('amqplib/callback_api');
+
+export interface Consumer {
+    name: string,
+    callback: Function,
+    options: any
+}
+
+export interface Queue {
+    name: string,
+    options: any
+}
+
+class AmqpClient {
+
+    private _connectionString: string = "";
+    private _connectionPending: Boolean = false;
+    private _statusQueueInitialized: Boolean = false;
+    private _connection: any = null;
+    private _channel: any = null;
+    private _queueList: Queue [] = [];
+    private _consumerList: Consumer [] = [];
+    private _logPrefix: string = "[AMQP_CLIENT]";
+    private _selfId: string;
+    private _statusQueue: string = "AMQP_CLIENT_STATUS";
+
+    //Public methods
+
+    constructor(conn_string: string = "", queue_list: Queue [] = [], consumer_list: Consumer [] = []) {
+        this._connectionString = conn_string;
+        this._queueList = queue_list;
+        this._consumerList = consumer_list;
+        this._selfId = this._createSelfId();
+    }   
+
+    setConnectionString(conn_string: string) {
+        this._connectionString = conn_string;
+    }
+    
+    setStatusQueueName(queue_name: string) {
+        this.addQueue(queue_name);
+        this._statusQueueInitialized = true;
+        this._statusQueue = queue_name;
+    }
+
+    addQueue(queue_name: string, options: any = { durable: false }) {
+        this._queueList.push({ name: queue_name, options: options });
+    }
+
+    addConsumer(queue_name: string, callback: Function, options: any = { noAck: true }) {
+        this._consumerList.push({ name: queue_name, callback: callback, options: options });
+    }
+
+    //Perform a connection with defined queues and consumers
+    async connect() {
+        let validate = true;
+        if (this._queueList.length == 0) {
+            validate = false;
+            this.log("[\x1b[31m✘\x1b[0m] Queue list shouldn't be empty when opening a new connection");
+        }
+        if (this._consumerList.length == 0) {
+            validate = false;
+            this.log("[\x1b[31m✘\x1b[0m] Consumer list shouldn't be empty when opening a new connection");
+        }
+        if (validate) {
+            this._connectionPending = true;
+            await amqp.connect(this._connectionString, (err: any, conn: any) => this.connectionCallback(err, conn));
+        }
+        if (!this._statusQueueInitialized) {
+            this.addQueue(this._statusQueue);
+        }
+    }
+
+    async sendMessage(queue: string, data: any) {
+        if (this._connection) {
+            try {
+                this._channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
+            } catch (err) {
+                this.log("[\x1b[31m✘] Error while sending message");
+                this._connection = null;
+                this._channel = null;
+                this.connect();
+            }
+        }
+    }
+
+    //Redefine this method, if you need to have your own logging
+    log(content: any, ...args: any []) {
+        console.log(this._logPrefix, content.toString(), ...args);
+    }
+
+    setLogPrefix(prefix: string) {
+        this._logPrefix = prefix;
+    }
+
+    async updateConnectionStatus() {
+        this._updateClientStatus();
+    }
+
+
+    //Private methods
+    
+    private connectionCallback(err: any, conn: any) {
+        if (err) {
+            this.log("Connection error: ", err.toString());
+        } else {
+            this._connection = conn;
+            this._connection.createChannel((err: any, channel: any) => this.channelCallback(err, channel));
+            this.log("[\x1b[32m✔\x1b[0m] Connection established");
+            this.log(`Client Self ID is \x1b[33m${this._selfId}\x1b[0m`);
+        }
+        this._connectionPending = false;
+    }
+
+    //This metod is called, when the connection is received in connect method
+    //Each pre-defined queue and consumer are asserted or connected to the channel
+    private channelCallback(err: any, channel: any) {
+        if (err) {
+            this.log("[\x1b[31m✘\x1b[0m] Error while creating channel: ", err.toString());
+        } else {
+            this._channel = channel;
+            //Connecting queues defined in list
+            this._queueList.forEach((queue: any) => {
+                this._channel.assertQueue(queue.name, queue.options);
+            });
+            //Connecting consumers defined in list
+            this._consumerList.forEach((consumer: any) => {
+                this._channel.consume(consumer.name, consumer.callback, consumer.options);
+            });
+        }
+    }
+    
+
+    private _updateClientStatus() {
+        if (this._connection) {
+            try {
+                this._channel.sendToQueue(this._statusQueue, Buffer.from(JSON.stringify({ clientId: this._selfId, dt: new Date() })));
+            } catch (err) {
+                this.log(err.toString());
+                this.log("[\x1b[31m✘\x1b[0m] Connection lost. Reconnecting");
+                this._connection = null;
+                this._channel = null;
+                this.connect();
+            }
+        } else if (!this._connectionPending) {
+            this._connection = null;
+            this._channel = null;
+            this.connect();
+        }
+    }
+
+    private _createSelfId(count=4) {
+        let s = [];
+        for (let i = 0; i < count; i++) {
+            s[i] = "0123456789abcdef".substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        let s_id = s.join("");
+        return s_id;
+    }
+}
+
+export default AmqpClient;
